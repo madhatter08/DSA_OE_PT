6c26ce2c96dff5488f5d89640a6839e3cb99937b
madhatter08 <90021594+madhatter08@users.noreply.github.com>
2022-12-11 16:05:44 +0800

    Update Manalo_PT4.java

--- PT4 Binary Tree/Manalo_PT4.java
+++ PT4 Binary Tree/Manalo_PT4.java
@@ -2,11 +2,11 @@
  * 	Name: Leah G. Manalo
  * 	PT4: Binary Tree implementation in Java. Following the menu selection below.
 
-			[1] In-order Traversal
-			[2] Pre-order Traversal
-			[3] Post-order Traversal
+		[1] In-order Traversal
+		[2] Pre-order Traversal
+		[3] Post-order Traversal
 			
-			Select option:
+		Select option:
  */
 //package com.dsa.datastructurealgorithm;
 import java.util.Scanner;
@@ -51,13 +51,13 @@ public class Manalo_PT4 {
 						case 1:
 							System.out.println("\n\tInorder Traversal with Static Data");
 							System.out.print("\tResult: ");
-					        inOrder(staticData);
-					        System.out.println();
+					        	inOrder(staticData);
+					        	System.out.println();
 							break;
-                        case 2:
-                        	System.out.println("\n\tInorder Traversal with User Data");
-                        	Node inputData = inputData();
-                            System.out.print("\tResult: ");
+                        			case 2:
+							System.out.println("\n\tInorder Traversal with User Data");
+							Node inputData = inputData();
+							System.out.print("\tResult: ");
 							inOrder(inputData);
 							System.out.println();
 							break;
@@ -81,13 +81,13 @@ public class Manalo_PT4 {
 						case 1:
 							System.out.println("\n\tPreorder Traversal with Static Data");
 							System.out.print("\tResult: ");
-					        preOrder(staticData);
-					        System.out.println();
+					        	preOrder(staticData);
+					        	System.out.println();
 							break;
 						case 2:
-                            System.out.println("\n\tPreorder Traversal with User Data");
-                            Node inputData = inputData();
-                            System.out.print("\tResult: ");
+						    	System.out.println("\n\tPreorder Traversal with User Data");
+						    	Node inputData = inputData();
+						    	System.out.print("\tResult: ");
 							preOrder(inputData);
 							System.out.println();
 							break;
@@ -111,13 +111,13 @@ public class Manalo_PT4 {
 						case 1:
 							System.out.println("\n\tPostorder Traversal with Static Data");
 							System.out.print("\tResult: ");
-					        postOrder(staticData);
-					        System.out.println();
+					        	postOrder(staticData);
+					        	System.out.println();
 							break;
 						case 2:
-                            System.out.println("\n\tPostorder Traversal with User Data");
-                            Node inputData = inputData();
-                            System.out.print("\tResult: ");
+						    	System.out.println("\n\tPostorder Traversal with User Data");
+						    	Node inputData = inputData();
+						    	System.out.print("\tResult: ");
 							postOrder(inputData);
 							System.out.println();
 							break;
@@ -139,81 +139,81 @@ public class Manalo_PT4 {
 	
 	//preorder traversal of data
 	public static void preOrder(Node node) {
-        if (node == null)
-            return;
-        System.out.print(node.value + " ");
-        preOrder(node.left);
-        preOrder(node.right);
-    }
-    
-	//inorder traversal of data
-    public static void inOrder(Node node) {
-        if (node == null)
-            return;
-        inOrder(node.left);
-        System.out.print(node.value + " ");
-        inOrder(node.right);
-    }
-    
-	//postorder traversal of data
-    public static void postOrder(Node node) {
-        if (node == null)
-            return;
-        postOrder(node.left);
-        postOrder(node.right);
-        System.out.print(node.value + " ");
-    }
-    
-    //create a list of static data
-    public static Node staticData() {
-        Node a = new Node(4);
-        Node b = new Node(2);
-        Node c = new Node(6);
-        Node d = new Node(1);
-        Node e = new Node(3);
-        Node f = new Node(5);
-        Node g = new Node(7);
-        a.left = b;
-        a.right = c;
-        b.left = d;
-        b.right = e;
-        c.left = f;
-        c.right = g;
-        return a;
+		if (node == null)
+		    return;
+		System.out.print(node.value + " ");
+		preOrder(node.left);
+		preOrder(node.right);
     	}
     
-    //prompt the user for data input
-    public static Node inputData() {
-    	Scanner input = new Scanner(System.in);
-    	System.out.print("\tEnter the value of the root node: ");
-        int rootValue = input.nextInt();
-    	System.out.print("\tEnter the value of the left child: ");
-        int leftValue = input.nextInt();
-        System.out.print("\tEnter the value of the right child: ");
-        int rightValue = input.nextInt();
-        System.out.print("\tEnter the value of the left.left child: ");
-        int leftleftValue = input.nextInt();
-        System.out.print("\tEnter the value of the left.right child: ");
-        int leftrightValue = input.nextInt();
-        System.out.print("\tEnter the value of the right.left child: ");
-        int rightleftValue = input.nextInt();
-        System.out.print("\tEnter the value of the right.right child: ");
-        int rightrightValue = input.nextInt();
-        
-        Node a = new Node(rootValue);
-        Node b = new Node(leftValue);
-        Node c = new Node(rightValue);
-        Node d = new Node(leftleftValue);
-        Node e = new Node(leftrightValue);
-        Node f = new Node(rightleftValue);
-        Node g = new Node(rightrightValue);
-        a.left = b;
-        a.right = c;
-        b.left = d;
-        b.right = e;
-        c.left = f;
-        c.right = g;
-        return a;
-    }
+	//inorder traversal of data
+    	public static void inOrder(Node node) {
+		if (node == null)
+		    return;
+		inOrder(node.left);
+		System.out.print(node.value + " ");
+		inOrder(node.right);
+    	}
+    
+	//postorder traversal of data
+    	public static void postOrder(Node node) {
+		if (node == null)
+		    return;
+		postOrder(node.left);
+		postOrder(node.right);
+		System.out.print(node.value + " ");
+    	}
+    
+    	//create a list of static data
+    	public static Node staticData() {
+		Node a = new Node(4);
+		Node b = new Node(2);
+		Node c = new Node(6);
+		Node d = new Node(1);
+		Node e = new Node(3);
+		Node f = new Node(5);
+		Node g = new Node(7);
+		a.left = b;
+		a.right = c;
+		b.left = d;
+		b.right = e;
+		c.left = f;
+		c.right = g;
+		return a;
+    	}
+    
+    	//prompt the user for data input
+    	public static Node inputData() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("\tEnter the value of the root node: ");
+		int rootValue = input.nextInt();
+		System.out.print("\tEnter the value of the left child: ");
+		int leftValue = input.nextInt();
+		System.out.print("\tEnter the value of the right child: ");
+		int rightValue = input.nextInt();
+		System.out.print("\tEnter the value of the left.left child: ");
+		int leftleftValue = input.nextInt();
+		System.out.print("\tEnter the value of the left.right child: ");
+		int leftrightValue = input.nextInt();
+		System.out.print("\tEnter the value of the right.left child: ");
+		int rightleftValue = input.nextInt();
+		System.out.print("\tEnter the value of the right.right child: ");
+		int rightrightValue = input.nextInt();
+
+		Node a = new Node(rootValue);
+		Node b = new Node(leftValue);
+		Node c = new Node(rightValue);
+		Node d = new Node(leftleftValue);
+		Node e = new Node(leftrightValue);
+		Node f = new Node(rightleftValue);
+		Node g = new Node(rightrightValue);
+		a.left = b;
+		a.right = c;
+		b.left = d;
+		b.right = e;
+		c.left = f;
+		c.right = g;
+		return a;
+    	}
 }
 
